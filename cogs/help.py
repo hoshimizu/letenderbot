61599d832dff4d9c0709065b6664384c1157b393
hoshimizu <hositamizu@yahoo.co.jp>
2026-08-19 08:21:56 +0900

    Update help command to remove unused commands
    Removed several commands from the help embed in help.py.

--- cogs/help.py
+++ cogs/help.py
@@ -11,15 +11,9 @@ class Help(commands.Cog):
     async def help(self, interaction: discord.Interaction):
         help_embed = discord.Embed(title='ヘルプ', color=discord.Color.blue())
         help_embed.add_field(name='/help', value='ヘルプを表示します。', inline=False)
-        help_embed.add_field(name='/ping', value='ボットの応答速度を表示します。', inline=False)
-        help_embed.add_field(name='/serverinfo', value='サーバーの情報を表示します。', inline=False)
-        help_embed.add_field(name='/userinfo', value='ユーザーの情報を表示します。', inline=False)
-        help_embed.add_field(name='/poll', value='投票を作成します。', inline=False)
         help_embed.add_field(name='/createinvite', value='招待リンクを作成します。', inline=False)
-        help_embed.add_field(name='/say', value='ボットに発言させます（管理者のみ）。', inline=False)
         help_embed.add_field(name='/welcomemessage', value='ウェルカムメッセージを設定します。', inline=False)
         help_embed.add_field(name='/welcomeoff', value='ウェルカムメッセージを削除します。', inline=False)
-        help_embed.add_field(name='/welcomecheck', value='現在のウェルカムメッセージ設定を確認します。', inline=False)
         await interaction.response.send_message(embed=help_embed, ephemeral=True)
 
 
